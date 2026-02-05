1ba672d6e47cce99f463ae10aee7f2e30a6a432d
Ashika <your_github_email@gmail.com>
2026-02-05 13:43:09 +0530

    Fix Render timeout issue

--- app.py
+++ app.py
@@ -12,5 +12,8 @@ def home():
     return jsonify({"message": "Honeypot API Tester is running"}), 200
 
 
+import os
+
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
