a0323c179e7e8d3f31b1ab1ac4f8a0aa20d23969
Ashika <your_github_email@gmail.com>
2026-02-05 13:24:17 +0530

    Fix GET endpoint for GUVI tester

--- app.py
+++ app.py
@@ -1,21 +1,16 @@
-from flask import Flask, jsonify
-import os
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["GET"])
 def home():
-    return jsonify({
-        "message": "Honeypot API Tester is running"
-    })
+    api_key = request.headers.get("x-api-key")
 
-@app.route("/health")
-def health():
-    return jsonify({
-        "status": "OK",
-        "service": "Honeypot API Tester"
-    })
+    if not api_key:
+        return jsonify({"error": "API key missing"}), 401
+
+    return jsonify({"message": "Honeypot API Tester is running"}), 200
+
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run()
